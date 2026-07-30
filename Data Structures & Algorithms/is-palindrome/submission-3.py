4ca54d5b717741b154439272df6d9ee19fffaf7c
Rafiat Amir <tut18432@temple.edu>
2026-07-30 14:24:13 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        #empty list to take input strings elements:numbers and letters only
+        clean =[]
+        
+        #loop to populate clean
+        for ch in s:
+            if ch.isalnum(): #boolean method that returns true if element is number/letter
+                clean.append(ch)
+        
+        s = "".join(clean).lower() #method to create a new string from list elements, lowercase cast
+        
+        n = len(s)
+        i = 0
+        j = n-1
+        p = (n-1)//2
+        k = 0
+
+        while i<=j:
+            if s[i] == s[j]:
+                i+=1
+                j-=1
+                k+=1
+            else:
+                return False
+        return True
